6a2e3df60de1c197aa743802f09bcfba29d92cb2
Insektaure <41437620+Insektaure@users.noreply.github.com>
2025-06-11 17:16:09 +0200

    feat: update localization keys and rename demo.json to pkdex.json

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,20 +1,3 @@
-/*
-    Copyright 2020-2021 natinusala
-    Copyright 2019 p-sam
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
-
 #if defined(ANDROID) || defined(IOS)
 #include <SDL2/SDL_main.h>
 #endif
@@ -60,7 +43,7 @@ int main(int argc, char* argv[])
         return EXIT_FAILURE;
     }
 
-    brls::Application::createWindow("demo/title"_i18n);
+    brls::Application::createWindow("pkdex/title"_i18n);
 
     brls::Application::getPlatform()->setThemeVariant(brls::ThemeVariant::DARK);
 
