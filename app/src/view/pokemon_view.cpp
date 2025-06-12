ea15dca7a6035ca2f6f5ac811d00c2396cbbe39f
Insektaure <41437620+Insektaure@users.noreply.github.com>
2025-06-12 17:47:29 +0200

    refactor: remove outdated copyright notices from multiple files

--- app/src/view/pokemon_view.cpp
+++ app/src/view/pokemon_view.cpp
@@ -1,19 +1,3 @@
-/*
-    Copyright 2021 XITRIX
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
-
 #include "view/pokemon_view.hpp"
 
 #include <borealis/core/i18n.hpp>
@@ -42,7 +26,7 @@ PokemonView::PokemonView(Pokemon pokemon, int pokemonIndex)
     };
 
     brls::Label* label = new brls::Label();
-    label->setText(brls::Hint::getKeyIcon(brls::ControllerButton::BUTTON_RB) + " Закрыть");
+    label->setText(brls::Hint::getKeyIcon(brls::ControllerButton::BUTTON_RB) + " Close");
     label->setFontSize(24);
     label->setMargins(0, 12, 0, 12);
 
